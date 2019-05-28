f55d3952558c81b197ef74cbb56d161091b22525
Dion Boles <dion.boles@gmail.com>
2019-05-28 12:23:37 -0400

    Added better sorting for select2

--- js/product.js
+++ js/product.js
@@ -1,3 +1,4 @@
+// object to hold product infomation
 var productObject = {
     id : [],
     name : [],
@@ -46,27 +47,69 @@ $(document).ready(function(){
         
         
      })
-
-
-     localStorage.setItem("dataset",JSON.stringify(productObject))
+     var sorting = $selection2.val();
+    //   made a function for the loop so it would save time  
+        function block2Loop(lowRange,hightRange){
+            productObject.price.forEach(function(e,i){
+                $(".block2").each(function(index){
+                    if(productObject.price[index] >= lowRange && productObject.price[index] <= hightRange){
+                            $(this).css("display","block");
+                    }else{
+                            $(this).css("display","none");
+                    }    
+                });
+                
+                });
+        }
+        $($selection2[1]).on("change",function(a){
+            var price  = $selection2.select2('data')[0].text;
+            switch (price) {
+                case "$0.00 - $50.00":
+                    block2Loop(0,50);
+                break;
+                case "$50.00 - $100.00":
+                    block2Loop(50,100);
+                break;
+                case "$100.00 - $150.00":
+                    block2Loop(100,150);
+                break;
+                case "$150.00 - $200.00":
+                    block2Loop(150,200);
+                break;
+                case "$200.00+":
+                        block2Loop(200,300);
+                break;
+                default:
+                    $(".block2").each(function(index){
+                        $(this).css("display","block"); 
+                    });
+                break;
+            }
+        });
+     localStorage.setItem("dataset",JSON.stringify(productObject));
 })
 window.onload = function(){
+    // ran
     document.getElementById('filter-bar').noUiSlider.on("end",function(){
-        if (document.getElementById("search-box").value != ""){
         var lowerValue = parseInt(String(document.getElementById("value-lower").innerText).replace("$","").trim());
         var upperValue = parseInt(String(document.getElementById("value-upper").innerText).replace("$","").trim());
+        if (lowerValue === 50 && upperValue == 200){
+            document.querySelectorAll(".block2").forEach(function(element,index){
+          
+                element.style.display = 'block';
+        });
+    }else{
         document.querySelectorAll(".block2").forEach(function(element,index){
-            if( lowerValue >= parseInt(productObject.price[index]) &&  parseInt(productObject.price[index]) <= upperValue){
+            if( parseInt(productObject.price[index]) >= lowerValue && parseInt(productObject.price[index]) <= upperValue){
                 element.style.display = 'block';
             }else{
                 element.style.display = 'none';
             }
         });
-    }else{
-        document.querySelectorAll(".block2").forEach(function(element,index){
-          
-                element.style.display = 'block';
-        });
+       
     }
     })
+
+
+
 }
